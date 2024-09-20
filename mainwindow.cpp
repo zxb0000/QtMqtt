2d58160863b1a1fa52b45b1cd0a470c0ac1f12de
zhangxiaobo <602975865@qq.com>
2024-09-20 13:45:27 +0800

    ui 和导入mqtt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,12 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    client=new QMqttClient();
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete client;
 }
-
